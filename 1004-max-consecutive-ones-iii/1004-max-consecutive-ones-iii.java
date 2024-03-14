04c4aea0b058631378218ca8443d90b684d041d5
Swathi Selvakumaran <73472775+SwathiSelvakumaran@users.noreply.github.com>
2024-03-14 14:28:53 -0400

    Time: 1452 ms (5.01%), Space: 47 MB (46.26%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -8,15 +8,10 @@ class Solution {
             if(temp!=0 && nums[r]==0){
                 sum = sum + 1;
                 temp--;
-                // System.out.println("sum 0:" + sum);
             } else if(nums[r] == 1){
                 sum = sum +1;
-                // System.out.println("sum 1:"+ sum);
             } else{
-                
-                // System.out.println("max:" + max);
                 sum = 0;
-                // System.out.println("reset sum:" + sum);
                 temp = k;
                 r=l;
                 l++;
@@ -24,11 +19,6 @@ class Solution {
             }
             max = Math.max(max,sum);
             
-            // while(<=sum){
-            //     max = Math.max(max, r-l+1);
-            //     sum = sum - nums[l];
-            //     l++;
-            // }
         }
         
         return max == Integer.MIN_VALUE && sum !=0 ? sum : max ;
